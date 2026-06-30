5532c1e3a091217d19363abdb320b523d408b2bb
Izhar Ahmad <izharahmadcreative@gmail.com>
2026-07-01 02:39:36 +0500

    Update firebaseConfig.ts

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -6,15 +6,6 @@ import { getStorage } from 'firebase/storage';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 // Your Firebase config from Firebase console
-export const firebaseConfig = {
-  apiKey: "AIzaSyBHH8gwlQRzwkVWMacA9gOVxxsBFt2tbC4",
-  authDomain: "nutrisense-5e2e8.firebaseapp.com",
-  projectId: "nutrisense-5e2e8",
-  storageBucket: "nutrisense-5e2e8.appspot.com",
-  messagingSenderId: "925478585043",
-  appId: "1:925478585043:web:63cb40248e84a7a782ca26",
-  // measurementId: "G-TJ2BDXF5X7", // optional
-};
 
 // Initialize Firebase app
 export const app = initializeApp(firebaseConfig);
